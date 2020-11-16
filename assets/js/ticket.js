9fac1179de84ae299af4bbbbea4ba62774f052a2
Diego <Petronacidiego@gmail.com>
2020-11-16 18:34:12 +0100

    progressione

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,11 +1,17 @@
 var btnGenera = document.getElementById("btn_genera");
 var btnAnnulla = document.getElementById("btn_annulla");
 
-btnGenera.addEventListener("Click", function () {
+btnGenera.addEventListener("click", function () {
   console.log("Genera il ticket");
 });
-btnAnnulla.addEventListener("Click", function () {
+btnAnnulla.addEventListener("click", function () {
   console.log("Annulla operazione");
 });
 
 var fullNameImput = document.getElementById("full_name");
+var kmImput = document.getElementById("km_totali");
+var fasciaImput = document.getElementById("fascia")
+
+//per collegare bottoni a js
+var fullName = fullNameImput.value;
+var km = kmImput.value;
