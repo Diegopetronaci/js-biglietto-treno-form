71546ab318592bb390b2939378c9ed8e9dfc91e5
Diego <Petronacidiego@gmail.com>
2020-11-16 16:54:19 +0100

    richiesta aiuto

---
+++ assets/js/ticket.js
@@ -0,0 +1,11 @@
+var btnGenera = document.getElementById("btn_genera");
+var btnAnnulla = document.getElementById("btn_annulla");
+
+btnGenera.addEventListener("Click", function () {
+  console.log("Genera il ticket");
+});
+btnAnnulla.addEventListener("Click", function () {
+  console.log("Annulla operazione");
+});
+
+var fullNameImput = document.getElementById("full_name");
